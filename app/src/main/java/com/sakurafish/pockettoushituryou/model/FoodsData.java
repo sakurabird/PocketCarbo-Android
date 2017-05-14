0f58e6cd7959ee2f0061e66911931e09d46e33df
Yukari Sakurai <neko3genki@gmail.com>
2017-05-14 13:13:41 +0900

    Update mojo

--- app/src/main/java/com/sakurafish/pockettoushituryou/model/FoodsData.java
+++ app/src/main/java/com/sakurafish/pockettoushituryou/model/FoodsData.java
@@ -1,102 +1,15 @@
 package com.sakurafish.pockettoushituryou.model;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import java.util.List;
 
-import java.util.ArrayList;
-
-public class FoodsData implements Parcelable {
-    public ArrayList<Foods> foods;
-
-    @Override
-    public int describeContents() {
-        return 0;
+public class FoodsData {
+    public List<Foods> getFoods() {
+        return foods;
     }
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeTypedList(foods);
-    }
-
-    public static final Parcelable.Creator<FoodsData> CREATOR = new Parcelable.Creator<FoodsData>() {
-        public FoodsData createFromParcel(Parcel in) {
-            return new FoodsData(in);
-        }
-
-        public FoodsData[] newArray(int size) {
-            return new FoodsData[size];
-        }
-    };
-
-    private FoodsData(Parcel in) {
-        foods = in.createTypedArrayList(Foods.CREATOR);
-    }
-
-    public FoodsData(ArrayList<Foods> foods) {
+    public void setFoods(List<Foods> foods) {
         this.foods = foods;
     }
 
-    public static class Foods implements Parcelable {
-        public int id;
-        public String name;
-        public int weight;
-        public String weight_hint;
-        public float carbohydrate_per_100g;
-        public float carbohydrate_per_weight;
-        public float calory;
-        public float protein;
-        public float fat;
-        public float sodium;
-        public String search_word;
-        public int type_id;
-        public int kind_id;
-
-        @Override
-        public int describeContents() {
-            return 0;
-        }
-
-        @Override
-        public void writeToParcel(Parcel dest, int flags) {
-            dest.writeInt(id);
-            dest.writeString(name);
-            dest.writeInt(weight);
-            dest.writeString(weight_hint);
-            dest.writeFloat(carbohydrate_per_100g);
-            dest.writeFloat(carbohydrate_per_weight);
-            dest.writeFloat(calory);
-            dest.writeFloat(protein);
-            dest.writeFloat(fat);
-            dest.writeFloat(sodium);
-            dest.writeString(search_word);
-            dest.writeInt(type_id);
-            dest.writeInt(kind_id);
-        }
-
-        public static final Creator<Foods> CREATOR = new Creator<Foods>() {
-            public Foods createFromParcel(Parcel in) {
-                return new Foods(in);
-            }
-
-            public Foods[] newArray(int size) {
-                return new Foods[size];
-            }
-        };
-
-        private Foods(Parcel in) {
-            id = in.readInt();
-            name = in.readString();
-            weight = in.readInt();
-            weight_hint = in.readString();
-            carbohydrate_per_100g = in.readFloat();
-            carbohydrate_per_weight = in.readFloat();
-            calory = in.readFloat();
-            protein = in.readFloat();
-            fat = in.readFloat();
-            sodium = in.readFloat();
-            search_word = in.readString();
-            type_id = in.readInt();
-            kind_id = in.readInt();
-        }
-    }
+    private List<Foods> foods;
 }
--- app/src/main/java/com/sakurafish/pockettoushituryou/model/KindsData.java
+++ app/src/main/java/com/sakurafish/pockettoushituryou/model/KindsData.java
@@ -1,73 +1,17 @@
 package com.sakurafish.pockettoushituryou.model;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import java.util.List;
 
-import java.util.ArrayList;
-
-public class KindsData implements Parcelable {
+public class KindsData {
     public static final int KINDS_ALL = 0;
-    public ArrayList<Kinds> kinds;
 
-    @Override
-    public int describeContents() {
-        return 0;
+    public List<Kinds> getKinds() {
+        return kinds;
     }
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeTypedList(kinds);
-    }
-
-    public static final Parcelable.Creator<KindsData> CREATOR = new Parcelable.Creator<KindsData>() {
-        public KindsData createFromParcel(Parcel in) {
-            return new KindsData(in);
-        }
-
-        public KindsData[] newArray(int size) {
-            return new KindsData[size];
-        }
-    };
-
-    private KindsData(Parcel in) {
-        kinds = in.createTypedArrayList(Kinds.CREATOR);
-    }
-
-    public KindsData(ArrayList<Kinds> kinds) {
+    public void setKinds(List<Kinds> kinds) {
         this.kinds = kinds;
     }
 
-    public static class Kinds implements Parcelable {
-        public int id;
-        public String name;
-        public int type_id;
-
-        @Override
-        public int describeContents() {
-            return 0;
-        }
-
-        @Override
-        public void writeToParcel(Parcel dest, int flags) {
-            dest.writeInt(id);
-            dest.writeString(name);
-            dest.writeInt(type_id);
-        }
-
-        public static final Creator<Kinds> CREATOR = new Creator<Kinds>() {
-            public Kinds createFromParcel(Parcel in) {
-                return new Kinds(in);
-            }
-
-            public Kinds[] newArray(int size) {
-                return new Kinds[size];
-            }
-        };
-
-        private Kinds(Parcel in) {
-            id = in.readInt();
-            name = in.readString();
-            type_id = in.readInt();
-        }
-    }
+    private List<Kinds> kinds;
 }
