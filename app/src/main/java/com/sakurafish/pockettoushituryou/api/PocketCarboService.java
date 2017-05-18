0e82bcb1d2bcdaf60ddf4e4787cc5e65ba0fb4c3
Yukari Sakurai <neko3genki@gmail.com>
2017-05-18 17:59:22 +0900

    Update network service definition and pojo

--- app/src/main/java/com/sakurafish/pockettoushituryou/api/PocketCarboService.java
+++ app/src/main/java/com/sakurafish/pockettoushituryou/api/PocketCarboService.java
@@ -1,15 +1,11 @@
 package com.sakurafish.pockettoushituryou.api;
 
 import com.sakurafish.pockettoushituryou.model.FoodsData;
-import com.sakurafish.pockettoushituryou.model.KindsData;
 
 import io.reactivex.Single;
 import retrofit2.http.GET;
 
 public interface PocketCarboService {
-
-    @GET("/api/v1/kinds")
-    Single<KindsData> getKindsData();
 
     @GET("/api/v1/foods")
     Single<FoodsData> getFoodsData();
--- app/src/main/java/com/sakurafish/pockettoushituryou/model/FoodsData.java
+++ app/src/main/java/com/sakurafish/pockettoushituryou/model/FoodsData.java
@@ -3,6 +3,19 @@ package com.sakurafish.pockettoushituryou.model;
 import java.util.List;
 
 public class FoodsData {
+    public static final int KINDS_ALL = 0;
+
+    private List<Kinds> kinds;
+    private List<Foods> foods;
+
+    public List<Kinds> getKinds() {
+        return kinds;
+    }
+
+    public void setKinds(List<Kinds> kinds) {
+        this.kinds = kinds;
+    }
+
     public List<Foods> getFoods() {
         return foods;
     }
@@ -10,6 +23,4 @@ public class FoodsData {
     public void setFoods(List<Foods> foods) {
         this.foods = foods;
     }
-
-    private List<Foods> foods;
 }
