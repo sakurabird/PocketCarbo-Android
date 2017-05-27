6fa564d0736374b3524a4f8c0c0aa4b7af6ccd8e
Yukari Sakurai <neko3genki@gmail.com>
2017-05-27 14:38:24 +0900

    Add basic menu(home, favorite, setting, share)

--- app/src/main/java/com/sakurafish/pockettoushituryou/view/activity/MainActivity.java
+++ app/src/main/java/com/sakurafish/pockettoushituryou/view/activity/MainActivity.java
@@ -167,20 +167,18 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                // Do nothing
+                break;
+            case R.id.nav_favorite:
+                break;
+            case R.id.nav_setting:
+                break;
+            case R.id.nav_share:
+                break;
+            case R.id.nav_help:
+                break;
         }
 
         binding.drawerLayout.closeDrawer(GravityCompat.START);
