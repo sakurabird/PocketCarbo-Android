8708564540ccd49ccfafcbbd350450579d370897
Yukari Sakurai <neko3genki@gmail.com>
2019-11-11 14:24:37 +0900

    Convert Java to Kotlin on TypesData & Make class type data class.

---
+++ app/src/main/java/com/sakurafish/pockettoushituryou/data/local/TypesData.kt
@@ -0,0 +1,13 @@
+package com.sakurafish.pockettoushituryou.data.local
+
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class TypesData(
+        val types: List<Type>
+)
+
+data class Type(
+        val id: Int,
+        val name: String?
+)
