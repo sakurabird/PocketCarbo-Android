91e8c7ee9cadf54342500359a62a4d0e844a8ca9
Yukari Sakurai <neko3genki@gmail.com>
2019-11-18 00:17:29 +0900

    Create AppCompatActivityExt & Delete BaseActivity.

--- app/src/main/java/com/sakurafish/pockettoushituryou/shared/ext/AppCompatActivityExt.kt
+++ app/src/main/java/com/sakurafish/pockettoushituryou/shared/ext/AppCompatActivityExt.kt
@@ -1,7 +1,10 @@
 package com.sakurafish.pockettoushituryou.shared.ext
 
+import android.content.Intent
+import android.net.Uri
 import androidx.annotation.IdRes
 import androidx.annotation.LayoutRes
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
@@ -9,4 +12,10 @@ fun AppCompatActivity.replaceFragment(fragment: Fragment, @IdRes @LayoutRes layo
     val ft = supportFragmentManager.beginTransaction()
     ft.replace(layoutResId, fragment, fragment.javaClass.simpleName)
     ft.commit()
+}
+
+fun AppCompatActivity.goBrowser(@NonNull url: String) {
+    val uri = Uri.parse(url)
+    val intent = Intent(Intent.ACTION_VIEW, uri)
+    startActivity(intent)
 }
