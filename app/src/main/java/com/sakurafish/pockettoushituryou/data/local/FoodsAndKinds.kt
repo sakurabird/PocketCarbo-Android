10ee4c9dd18b245beaf51098e6fe9a240fb0aa60
Yukari Sakurai <neko3genki@gmail.com>
2020-04-14 16:22:40 +0900

    Add Room db entity classes

---
+++ app/src/main/java/com/sakurafish/pockettoushituryou/data/local/FoodsAndKinds.kt
@@ -0,0 +1,17 @@
+package com.sakurafish.pockettoushituryou.data.local
+
+import com.sakurafish.pockettoushituryou.data.db.entity.Food
+import com.sakurafish.pockettoushituryou.data.db.entity.Kind
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+/**
+ * Entity class for Local data ('assets/json/foods_and_kinds.json') since version 3.5.1
+ */
+@JsonClass(generateAdapter = true)
+data class FoodsAndKinds(
+        @Json(name = "data_version")
+        var dataVersion: Int = 0,
+        var kinds: List<Kind>? = null,
+        var foods: List<Food>? = null
+)
