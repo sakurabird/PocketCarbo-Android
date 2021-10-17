01c7d76045b07f657e0d5325c1ccf39093d072e0
Yukari Sakurai <neko3genki@gmail.com>
2021-10-18 00:34:08 +0900

    Convert Java to Kotlin using auto converter

---
+++ app/src/main/java/com/sakurafish/pockettoushituryou/receiver/NotificationReceiver.kt
@@ -0,0 +1,92 @@
+package com.sakurafish.pockettoushituryou.receiver
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import androidx.preference.PreferenceManager
+import com.sakurafish.pockettoushituryou.R
+import com.sakurafish.pockettoushituryou.view.activity.MainActivity
+
+class NotificationReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        // 通知が設定画面から許可されていなかったら通知しない
+        val preference = PreferenceManager.getDefaultSharedPreferences(context)
+        if (!preference.getBoolean(context.getString(R.string.PREF_NOTIFICATION), true)) {
+            return
+        }
+        val lastNo =
+            preference.getInt(context.getString(R.string.PREF_APP_MESSAGE_NO), 0) // 通知済みメッセージ番号
+        val messageNo =
+            context.resources.getInteger(R.integer.APP_MESSAGE_NO) // consts.xmlで定義されている数字
+
+        // 既に通知済みのメッセージなら通知しない
+        if (messageNo <= lastNo) {
+            return
+        }
+        showNotification(context, preference, messageNo)
+    }
+
+    private fun showNotification(context: Context, preference: SharedPreferences, messageNo: Int) {
+        val title = context.getString(R.string.notification_title)
+        val text = context.getString(R.string.APP_MESSAGE_TEXT)
+        val openIntent = MainActivity.createIntent(context)
+        openIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        val pendingIntent =
+            PendingIntent.getActivity(context, 0, openIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                ?: return
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                context.getString(R.string.notification_channel_name),
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationChannel.description =
+                context.getString(R.string.notification_channel_description)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val builder = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+        val mNotification = builder
+            .setTicker(title)
+            .setContentTitle(title)
+            .setContentText(text)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText(text)
+            )
+            .setSmallIcon(R.drawable.ic_notification)
+            .setLargeIcon(BitmapFactory.decodeResource(context.resources, R.mipmap.ic_launcher))
+            .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setPriority(Notification.PRIORITY_DEFAULT)
+            .setWhen(System.currentTimeMillis())
+            .build()
+        notificationManager.notify(NOTIFICATION_ID, mNotification)
+        val editor = preference.edit()
+        editor.putInt(context.getString(R.string.PREF_APP_MESSAGE_NO), messageNo)
+        editor.apply()
+    }
+
+    companion object {
+        private val TAG = NotificationReceiver::class.java.simpleName
+        const val NOTIFICATION_CHANNEL_ID = "pocketcarbo_channel_id_01"
+        const val NOTIFICATION_ID = 33
+
+        @JvmStatic
+        fun createIntent(context: Context?): Intent {
+            return Intent(context, NotificationReceiver::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/sakurafish/pockettoushituryou/shared/AlarmUtils.kt
@@ -0,0 +1,50 @@
+package com.sakurafish.pockettoushituryou.shared
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import com.sakurafish.pockettoushituryou.receiver.NotificationReceiver
+import java.util.*
+
+object AlarmUtils {
+    private val TAG = AlarmUtils::class.java.simpleName
+
+    @JvmStatic
+    fun registerAlarm(context: Context) {
+        // Wake Up every day at PM12:00 for app message
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            ?: return
+
+        // every day at scheduled time
+        val calendar = Calendar.getInstance()
+        // if it's after or equal 12 pm schedule for next day
+        if (Calendar.getInstance()[Calendar.HOUR_OF_DAY] >= 12) {
+            calendar.add(Calendar.DAY_OF_YEAR, 1)
+        }
+        calendar[Calendar.HOUR_OF_DAY] = 12
+        calendar[Calendar.MINUTE] = 0
+        calendar[Calendar.SECOND] = 0
+        val pendingIntent = createAlarmIntent(context)
+        alarmManager.setInexactRepeating(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent
+        )
+    }
+
+    @JvmStatic
+    fun unregisterAlarm(context: Context) {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            ?: return
+        val pendingIntent = createAlarmIntent(context)
+        pendingIntent.cancel()
+        alarmManager.cancel(pendingIntent)
+    }
+
+    private fun createAlarmIntent(context: Context): PendingIntent {
+        val pendingIntent = NotificationReceiver.createIntent(context)
+        return PendingIntent.getBroadcast(
+            context, NotificationReceiver.NOTIFICATION_ID,
+            pendingIntent, PendingIntent.FLAG_UPDATE_CURRENT
+        )
+    }
+}
